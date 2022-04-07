c464553675fbfc007c0379afbcc2b43b0e1c43ad
Ryan Harter <rharter@dropbox.com>
2022-04-07 11:24:15 -0500

    Adds plugin tests.

---
+++ focus-gradle-plugin/src/test/kotlin/com/dropbox/focus/FocusPluginTest.kt
@@ -0,0 +1,52 @@
+package com.dropbox.focus
+
+import com.google.common.truth.Truth.assertThat
+import java.io.File
+import org.gradle.testkit.runner.BuildResult
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.Before
+import org.junit.Test
+class FocusPluginTest {
+  private lateinit var gradleRunner: GradleRunner
+
+  @Before
+  fun setup() {
+    gradleRunner = GradleRunner.create()
+      .withPluginClasspath()
+  }
+
+  @Test
+  fun configurationCache() {
+    val fixtureRoot = File("src/test/projects/configuration-cache-compatible")
+
+    gradleRunner
+      .withArguments("clearFocus", "--configuration-cache", "--stacktrace")
+      .runFixture(fixtureRoot) { build() }
+  }
+
+  @Test
+  fun missingSettingsAll() {
+    val fixtureRoot = File("src/test/projects/missing-settings-all")
+
+    val firstRun = gradleRunner
+      .withArguments("clearFocus")
+      .runFixture(fixtureRoot) { buildAndFail() }
+
+    assertThat(firstRun.output).contains("Could not read script")
+    assertThat(firstRun.output).contains("as it does not exist.")
+  }
+
+  private fun GradleRunner.runFixture(
+    projectRoot: File,
+    moduleRoot: File = projectRoot,
+    action: GradleRunner.() -> BuildResult,
+  ): BuildResult {
+    val settings = File(projectRoot, "settings.gradle")
+    if (!settings.exists()) {
+      settings.createNewFile()
+      settings.deleteOnExit()
+    }
+
+    return withProjectDir(projectRoot).action()
+  }
+}
